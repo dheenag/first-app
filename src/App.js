8d3e24ff09681c2cf1f6183e23386a1946c7fab1
Dinakaran G <dg@healthgrades.com>
2020-02-25 15:21:30 +0530

    Added Search Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyDemo from './components/DemoComponent';
+//import MyGitClassComp from './components/GithubCommitComponent';
+import MyGitFunComp from './components/GithubCommitFunComponent';
+import Search from './components/Search/Search';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <MyDemo />
+        <Search />
+        <div>
+          {/* <MyGitClassComp/> */}
+
+          <MyGitFunComp />
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Demo <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Search = () => {
+    return (          
+        <div><h1>Hello Search Component</h1> </div>
+    );
+}
+
+export default Search;
